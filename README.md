e21439a415bb926c20ccce8a7db605771cc57089
Jim Pick <jim@jimpick.com>
2020-10-01 13:27:41 -0700

    Basic ping in browser example

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Based on:
 
 * [https://github.com/libp2p/js-libp2p/tree/master/examples/libp2p-in-the-browser](https://github.com/libp2p/js-libp2p/tree/master/examples/libp2p-in-the-browser)
 * [Getting started with go-libp2p](https://docs.libp2p.io/tutorials/getting-started/go/)
+* [Getting started with js-libp2p](https://docs.libp2p.io/tutorials/getting-started/javascript/)
 
 ## License
 
--- index.js
+++ index.js
@@ -1,46 +1,16 @@
 import 'babel-polyfill'
 import Libp2p from 'libp2p'
 import Websockets from 'libp2p-websockets'
-import WebRTCStar from 'libp2p-webrtc-star'
 import { NOISE } from 'libp2p-noise'
-import Secio from 'libp2p-secio'
 import Mplex from 'libp2p-mplex'
-import Bootstrap from 'libp2p-bootstrap'
 
 document.addEventListener('DOMContentLoaded', async () => {
   // Create our libp2p node
   const libp2p = await Libp2p.create({
-    addresses: {
-      // Add the signaling server address, along with our PeerId to our multiaddrs list
-      // libp2p will automatically attempt to dial to the signaling server so that it can
-      // receive inbound connections from other peers
-      listen: [
-        '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
-        '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star'
-      ]
-    },
     modules: {
-      transport: [Websockets, WebRTCStar],
-      connEncryption: [NOISE, Secio],
-      streamMuxer: [Mplex],
-      peerDiscovery: [Bootstrap]
-    },
-    config: {
-      peerDiscovery: {
-        // The `tag` property will be searched when creating the instance of your Peer Discovery service.
-        // The associated object, will be passed to the service when it is instantiated.
-        [Bootstrap.tag]: {
-          enabled: true,
-          list: [
-            '/dns4/ams-1.bootstrap.libp2p.io/tcp/443/wss/p2p/QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd',
-            '/dns4/lon-1.bootstrap.libp2p.io/tcp/443/wss/p2p/QmSoLMeWqB7YGVLJN3pNLQpmmEk35v6wYtsMGLzSr5QBU3',
-            '/dns4/sfo-3.bootstrap.libp2p.io/tcp/443/wss/p2p/QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM',
-            '/dns4/sgp-1.bootstrap.libp2p.io/tcp/443/wss/p2p/QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu',
-            '/dns4/nyc-1.bootstrap.libp2p.io/tcp/443/wss/p2p/QmSoLueR4xBeUbY9WZ9xGUUxunbKWcrNFTDAadQJmocnWm',
-            '/dns4/nyc-2.bootstrap.libp2p.io/tcp/443/wss/p2p/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64'
-          ]
-        }
-      }
+      transport: [Websockets],
+      connEncryption: [NOISE],
+      streamMuxer: [Mplex]
     }
   })
 
@@ -55,24 +25,27 @@ document.addEventListener('DOMContentLoaded', async () => {
     output.textContent += `${txt.trim()}\n`
   }
 
-  // Listen for new peers
-  libp2p.on('peer:discovery', (peerId) => {
-    log(`Found peer ${peerId.toB58String()}`)
-  })
-
   // Listen for new connections to peers
-  libp2p.connectionManager.on('peer:connect', (connection) => {
+  libp2p.connectionManager.on('peer:connect', connection => {
     log(`Connected to ${connection.remotePeer.toB58String()}`)
   })
 
   // Listen for peers disconnecting
-  libp2p.connectionManager.on('peer:disconnect', (connection) => {
+  libp2p.connectionManager.on('peer:disconnect', connection => {
     log(`Disconnected from ${connection.remotePeer.toB58String()}`)
   })
 
   await libp2p.start()
   status.innerText = 'libp2p started!'
   log(`libp2p id is ${libp2p.peerId.toB58String()}`)
+
+  const button = document.querySelector('#pingBtn')
+  button.onclick = async function () {
+    const target = document.querySelector('#maddr').value
+    log(`Ping: ${target}`)
+    const latency = await libp2p.ping(target)
+    log(`Pong: ${latency}ms`)
+  }
 
   // Export libp2p to the window so you can play with the API
   window.libp2p = libp2p
