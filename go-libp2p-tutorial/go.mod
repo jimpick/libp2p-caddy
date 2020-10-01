3d1216111ee9e75ff42aa7c5a0a44b9087150518
Jim Pick <jim@jimpick.com>
2020-10-01 12:13:27 -0700

    Basic TCP go-libp2p node with ping support

--- go-libp2p-tutorial/go.mod
+++ go-libp2p-tutorial/go.mod
@@ -2,4 +2,7 @@ module github.com/jimpick/libp2p-caddy/go-libp2p-tutorial
 
 go 1.14
 
-require github.com/libp2p/go-libp2p v0.11.0
+require (
+	github.com/libp2p/go-libp2p v0.11.0
+	github.com/libp2p/go-libp2p-core v0.6.1
+)
--- go-libp2p-tutorial/main.go
+++ go-libp2p-tutorial/main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/libp2p/go-libp2p"
+	peerstore "github.com/libp2p/go-libp2p-core/peer"
 )
 
 func main() {
@@ -12,7 +16,9 @@ func main() {
 	ctx := context.Background()
 
 	// start a libp2p node with default settings
-	node, err := libp2p.New(ctx)
+	node, err := libp2p.New(ctx,
+		libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/2000"),
+	)
 	if err != nil {
 		panic(err)
 	}
@@ -20,9 +26,22 @@ func main() {
 	// print the node's listening addresses
 	fmt.Println("Listen addresses:", node.Addrs())
 
+	// print the node's PeerInfo in multiaddr format
+	peerInfo := peerstore.AddrInfo{
+		ID:    node.ID(),
+		Addrs: node.Addrs(),
+	}
+	addrs, err := peerstore.AddrInfoToP2pAddrs(&peerInfo)
+	fmt.Println("libp2p node address:", addrs[0])
+
+	// wait for a SIGINT or SIGTERM signal
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	<-ch
+	fmt.Println("Received signal, shutting down...")
+
 	// shut the node down
 	if err := node.Close(); err != nil {
 		panic(err)
 	}
 }
-
