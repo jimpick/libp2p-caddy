fdf18b2d03b4c870bff72d02aab87a8853a30171
Jim Pick <jim@jimpick.com>
2020-10-16 19:20:06 -0700

    Get result working in browser by not using channels

--- go-wasm/main.go
+++ go-wasm/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"syscall/js"
+	"time"
 
 	"github.com/libp2p/go-libp2p"
 	peerstore "github.com/libp2p/go-libp2p-core/peer"
@@ -13,24 +14,18 @@ import (
 )
 
 var (
-	ch         <-chan ping.Result
-	sendPingCh chan string
-	pingCh     chan ping.Result
+	ch           <-chan ping.Result
+	pingResultCh chan ping.Result
+	pingMaddr    string
 )
-
-/*
-func add(this js.Value, i []js.Value) interface{} {
-	result := js.ValueOf(i[0].Int() + i[1].Int())
-	println(result.String())
-	return js.ValueOf(result)
-}
-*/
 
 func pingNode(this js.Value, param []js.Value) interface{} {
 	maddr := param[0].String()
 	println("Go maddr: ", maddr)
-	sendPingCh <- maddr
-	res := <-pingCh
+	pingMaddr = maddr
+	return js.ValueOf(1234)
+
+	// res := <-pingResultCh
 
 	// create a background context (i.e. one that never cancels)
 	// ctx := context.Background()
@@ -64,8 +59,10 @@ func pingNode(this js.Value, param []js.Value) interface{} {
 	*/
 
 	// res := <-ch
-	fmt.Println("click pinged", maddr, "in", res.RTT)
-	return js.ValueOf(res.RTT.Seconds())
+	/*
+		fmt.Println("click pinged", maddr, "in", res.RTT)
+		return js.ValueOf(res.RTT.Milliseconds())
+	*/
 }
 
 func main() {
@@ -86,54 +83,60 @@ func main() {
 	pingService := &ping.PingService{Host: node}
 	node.SetStreamHandler(ping.ID, pingService.PingHandler)
 
-	/*
-		maddr := "/dns4/libp2p-caddy-ws.localhost/tcp/9056/wss/p2p/QmRvgKx6ffb9SNJBbVWTqhtZWVM36cf7zB65k8CdywPRXS"
-		addr, err := multiaddr.NewMultiaddr(maddr)
-		if err != nil {
-			panic(err)
-		}
-		peer, err := peerstore.AddrInfoFromP2pAddr(addr)
-		if err != nil {
-			panic(err)
-		}
-		if err := node.Connect(ctx, *peer); err != nil {
-			panic(err)
-		}
-		go func() {
-			fmt.Println("sending ping messages to", addr)
-			ch = pingService.Ping(ctx, peer.ID)
-			for i := 0; i < 500; i++ {
+	pingResultCh = make(chan ping.Result)
+
+	go func() {
+		for {
+			if pingMaddr != "" {
+				// maddr := "/dns4/libp2p-caddy-ws.localhost/tcp/9056/wss/p2p/QmRvgKx6ffb9SNJBbVWTqhtZWVM36cf7zB65k8CdywPRXS"
+				fmt.Printf("Jim ping maddr found: %v\n", pingMaddr)
+				addr, err := multiaddr.NewMultiaddr(pingMaddr)
+				if err != nil {
+					panic(err)
+				}
+				peer, err := peerstore.AddrInfoFromP2pAddr(addr)
+				if err != nil {
+					panic(err)
+				}
+				if err := node.Connect(ctx, *peer); err != nil {
+					panic(err)
+				}
+				fmt.Println("sending ping messages to", addr)
+				ch = pingService.Ping(ctx, peer.ID)
 				res := <-ch
-				fmt.Println(i, "pinged", addr, "in", res.RTT)
-				time.Sleep(10 * time.Second)
+				fmt.Println("pinged", addr, "in", res.RTT)
+				// pingResultCh <- res
+				pingMaddr = ""
+			}
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
+	/*
+		sendPingCh = make(chan string)
+		go func() {
+			for {
+				maddr := <-sendPingCh
+				fmt.Println("request to ping", maddr)
+				addr, err := multiaddr.NewMultiaddr(maddr)
+				if err != nil {
+					panic(err)
+				}
+				peer, err := peerstore.AddrInfoFromP2pAddr(addr)
+				if err != nil {
+					panic(err)
+				}
+				if err := node.Connect(ctx, *peer); err != nil {
+					panic(err)
+				}
+				fmt.Println("connected, pinging", peer.ID)
+				newPingCh := pingService.Ping(ctx, peer.ID)
+				res := <-newPingCh
+				fmt.Println("pinged", addr, "in", res.RTT)
+				pingCh <- res
 			}
 		}()
 	*/
-
-	sendPingCh = make(chan string)
-	pingCh = make(chan ping.Result)
-	go func() {
-		for {
-			maddr := <-sendPingCh
-			fmt.Println("request to ping", maddr)
-			addr, err := multiaddr.NewMultiaddr(maddr)
-			if err != nil {
-				panic(err)
-			}
-			peer, err := peerstore.AddrInfoFromP2pAddr(addr)
-			if err != nil {
-				panic(err)
-			}
-			if err := node.Connect(ctx, *peer); err != nil {
-				panic(err)
-			}
-			fmt.Println("connected, pinging", peer.ID)
-			newPingCh := pingService.Ping(ctx, peer.ID)
-			res := <-newPingCh
-			fmt.Println("pinged", addr, "in", res.RTT)
-			pingCh <- res
-		}
-	}()
 
 	js.Global().Set("ping", js.FuncOf(pingNode))
 
