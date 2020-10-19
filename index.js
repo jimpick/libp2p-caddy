f59e5a66162c9bfff60bd26e88aafe5a20cedaf0
Jim Pick <jim@jimpick.com>
2020-10-19 14:39:04 -0700

    Complete wasm demo

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-import 'babel-polyfill'
-import Libp2p from 'libp2p'
-import Websockets from 'libp2p-websockets'
-import { NOISE } from 'libp2p-noise'
-import Mplex from 'libp2p-mplex'
+import pako from 'pako'
 
 document.addEventListener('DOMContentLoaded', async () => {
-  // Create our libp2p node
-  const libp2p = await Libp2p.create({
-    modules: {
-      transport: [Websockets],
-      connEncryption: [NOISE],
-      streamMuxer: [Mplex]
-    }
-  })
-
   // UI elements
   const status = document.getElementById('status')
   const output = document.getElementById('output')
@@ -25,47 +12,20 @@ document.addEventListener('DOMContentLoaded', async () => {
     output.textContent += `${txt.trim()}\n`
   }
 
-  // Listen for new connections to peers
-  libp2p.connectionManager.on('peer:connect', connection => {
-    log(`JS Connected to ${connection.remotePeer.toB58String()}`)
-  })
+  status.innerText = 'Loading Go WASM bundle...'
 
-  // Listen for peers disconnecting
-  libp2p.connectionManager.on('peer:disconnect', connection => {
-    log(`Disconnected from ${connection.remotePeer.toB58String()}`)
-  })
-
-  await libp2p.start()
-  status.innerText = 'js libp2p started, loading go WASM bundle...'
-  log(`JS libp2p id is ${libp2p.peerId.toB58String()}`)
-
-  const jsPingButton = document.querySelector('#jsPingBtn')
-  jsPingButton.disabled = false
-  jsPingButton.onclick = async function () {
-    const target = document.querySelector('#maddr').value
-    log(`JS Ping: ${target}`)
-    const latency = await libp2p.ping(target)
-    log(`JS Pong: ${latency}ms`)
-  }
-
-  const setIpfsMaddrButton = document.querySelector('#setIpfsMaddrBtn')
-  setIpfsMaddrButton.onclick = async function () {
-    document.querySelector('#maddr').value =
-      '/dns4/libp2p-caddy-ipfs.localhost/tcp/9057/wss/p2p/QmScdku7gc3VvfZZvT8kHU77bt6bnH3PnGXkyFRZ17g9EG'
-  }
-
-  // Export libp2p to the window so you can play with the API
-  window.libp2p = libp2p
-  console.log('.env PEER_ID:', process.env.PEER_ID)
-  document.querySelector('#maddr').value =
-    '/dns4/libp2p-caddy-ws.localhost/tcp/9056/wss/p2p/' + process.env.PEER_ID
+  // Use gzip: https://dstoiko.github.io/posts/go-pong-wasm/
 
   const go = new Go()
-  WebAssembly.instantiateStreaming(
-    fetch('go-wasm/main.wasm'),
-    go.importObject
-  ).then(result => {
-    status.innerText = 'All systems good! JS and Go loaded.'
+  const url = 'go-wasm/main.wasm.gz' // the gzip-compressed wasm file
+  let wasm = pako.ungzip(await (await fetch(url)).arrayBuffer())
+  // A fetched response might be decompressed twice on Firefox.
+  // See https://bugzilla.mozilla.org/show_bug.cgi?id=610679
+  if (wasm[0] === 0x1f && wasm[1] === 0x8b) {
+    wasm = pako.ungzip(wasm)
+  }
+  WebAssembly.instantiate(wasm, go.importObject).then(result => {
+    status.innerText = 'All systems good! Go WASM loaded.'
     go.run(result.instance)
 
     // FIXME Add callback for ready state
@@ -93,6 +53,5 @@ document.addEventListener('DOMContentLoaded', async () => {
       const data = await window.graphSyncFetch(target, cid)
       log(`> Data:\n${data}\n`)
     }
-
   })
 })
